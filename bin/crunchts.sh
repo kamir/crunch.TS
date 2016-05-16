5e6ec9c354e79abf1d752e39438f2caba581a8d2
Mirko <mirko.kaempf@gmail.com>
2016-05-16 19:03:04 +0200

    synch

--- bin/crunchts.sh
+++ bin/crunchts.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-echo "CMD   : $1"
-echo "Input : $2/$3"
-echo "Output: $4"
+export P1=-explode
+export P2=/TSBASE/EXP1
+export P3=Components_DAX2_Close__2003_2004_2005_2006_2007_2008_2009_2010_2011_2012_2013_2014.tsb.vec.seq
+export P4=pairs.avro
 
-hadoop jar target/crunchTS-0.0.1-SNAPSHOT-job.jar $1 $2/$3 $4 
+echo "CMD   : $P1"
+echo "Input : $P2/$P3"
+echo "Output: $P4"
+
+hadoop jar target/SparkNetworkCreator-0.1.0-SNAPSHOT-job $1 $2/$3 $4 
